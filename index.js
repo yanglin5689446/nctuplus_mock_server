c1a0519232517ff46b875f76767d2a873e9020f5
Johnson Chao <chao.hs1031@gmail.com>
2018-04-23 12:41:34 +0800

    Add wrapper before render.
    Add package url-pattern to parse url
    Replace json-server router.render
    Wrap certain json response per api specs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jsonServer = require('json-server')
 const chalk = require('chalk')
 const routes = require('./route.json')
 const seed = require('./seed/index.js')
+const render = require('./render')
 
 const config = {
   host: '0.0.0.0',
@@ -20,22 +21,26 @@ server.use(middlewares)
 server.use(function(req,res,next){setTimeout(next, config.delay)}); // Add delay for all requests
 server.use(router)
 
-function prettyPrint(argv, object, rules) {
-  const host = argv.host === '0.0.0.0' ? 'localhost' : argv.host
-  const port = argv.port
+router.render = render
+
+server.listen(config.port, config.host, () => {
+
+  // Output connection message
+  const host = config.host === '0.0.0.0' ? 'localhost' : config.host
+  const port = config.port
   const root = `http://${host}:${port}`
 
   console.log()
   console.log(chalk.bold('  Resources'))
-  for (let prop in object) {
+  for (let prop in data) {
     console.log(`  ${root}/${prop}`)
   }
 
-  if (rules) {
+  if (routes) {
     console.log()
     console.log(chalk.bold('  Other routes'))
-    for (var rule in rules) {
-      console.log(`  ${rule} -> ${rules[rule]}`)
+    for (var rule in routes) {
+      console.log(`  ${rule} -> ${routes[rule]}`)
     }
   }
 
@@ -43,8 +48,4 @@ function prettyPrint(argv, object, rules) {
   console.log(chalk.bold('  Home'))
   console.log(`  ${root}`)
   console.log()
-}
-
-server.listen(config.port, config.host, () => {
-  prettyPrint(config, data, routes)
 })
